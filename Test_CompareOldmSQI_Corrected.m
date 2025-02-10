2f40a59dfad421201cb0cb1fa14ac9f4d1d38915
AbrahamOtero <abraham.otero@gmail.com>
2025-02-10 10:59:09 +0100

    Añadida función para comparar mSQI antiguo y viejo a partir de los archivos

---
+++ Test_CompareOldmSQI_Corrected.m
@@ -0,0 +1,6 @@
+archivos_mSQI_original = {'mSQI_Top_NewIII_Working_2024-10-18.txt.csv', 'mSQI_Top_NewIII_Stairs_2024-10-18.txt.csv'};
+archivos_mSQI_corregido_arm = {'CorrectionmSQITopNewIIIWorking20241018_csvPowerArmNewIIIWorking.csv', 'CorrectionmSQITopNewIIIStairs20241018_csvPowerArmNewIIIStairs20.csv'};
+archivos_mSQI_corregido_sternum = {'CorrectionmSQITopNewIIIWorking20241018_csvPowerSternumNewIIIWor.csv', 'CorrectionmSQITopNewIIIStairs20241018_csvPowerSternumNewIIIStai.csv'}; 
+
+
+Compare_OldmSQI_CorrectedmSQI(archivos_mSQI_original, archivos_mSQI_corregido_arm, archivos_mSQI_corregido_sternum)
